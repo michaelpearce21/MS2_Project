4e83e8f33ffb07d1adf0923f88d1cc5924465674
Michael Pearce <michaelpearce@Michaels-Air.broadband>
2021-09-18 15:57:33 +0100

    fix: testing is quiz displays as intended

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,54 +1,112 @@
-// constants
-
-// Linking to the posiiton on the HTML
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-// Questions stored as an array. Positive is it's easy to iterate over this way. 
-const questions = [
-    {
-        question: "Who was the first AEW World Champion?",
+(function(){
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>`
+          );
+        }
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
+        }
+      });
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "Who invented JavaScript?",
         answers: {
-            a: "Chris Jericho",
-            b: "Jon Moxley",
-            c: "Brandon Cutler",
-            d: "Adam Page"
+          a: "Douglas Crockford",
+          b: "Sheryl Sandberg",
+          c: "Brendan Eich"
         },
-        correctAnswer: "a"
-    },
-    {
-        question: "Who were the first AEW Tag Team Champions?",
+        correctAnswer: "c"
+      },
+      {
+        question: "Which one of these is a JavaScript package manager?",
         answers: {
-            a: "The Young Bucks",
-            b: "The Lucha Brothers",
-            c: "Alex Reynolds and John Silver",
-            d: "SCU"
+          a: "Node.js",
+          b: "TypeScript",
+          c: "npm"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+          a: "Angular",
+          b: "jQuery",
+          c: "RequireJS",
+          d: "ESLint"
         },
         correctAnswer: "d"
-    },
-    {
-        question: "Who was the first AEW Womens World Champion?",
-        answers: {
-            a: "Nyla Rose",
-            b: "Riho",
-            c: "Britt Baker",
-            d: "Thunder Rose"
-        },
-        correctAnswer: "b"
-    },
-]
-
-// functions
-
-//
-function createQuiz(){}
-
-function showResults(){}
-
-// To display straight away. 
-createQuiz();
-
-
-// To show results on submit
-submitButton.addEventListener('click', showResults);
+      }
+    ];
+  
+    // Kick things off
+    buildQuiz();
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+  })();
