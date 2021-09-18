3e1aa8d74f6f3a76955803eb54c75c9cc09c1bd5
Michael Pearce <michaelpearce@Michaels-Air.broadband>
2021-09-18 15:38:39 +0100

    feat: added sample questions for later testing

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,44 @@
+// constants
+
+// Linking to the posiiton on the HTML
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+// Questions stored as an array. Positive is it's easy to iterate over this way. 
+const questions = [
+    {
+        question: "Who was the first AEW World Champion?",
+        answers: {
+            a: "Chris Jericho",
+            b: "Jon Moxley",
+            c: "Brandon Cutler",
+            d: "Adam Page"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Who were the first AEW Tag Team Champions?",
+        answers: {
+            a: "The Young Bucks",
+            b: "The Lucha Brothers",
+            c: "Alex Reynolds and John Silver",
+            d: "SCU"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Who was the first AEW Womens World Champion?",
+        answers: {
+            a: "Nyla Rose",
+            b: "Riho",
+            c: "Britt Baker",
+            d: "Thunder Rose"
+        },
+        correctAnswer: "b"
+    },
+]
+
 // functions
 
 //
