e110149bb4c22a592ab67bdb5e41e263e4ca4a9b
Michael Pearce <michaelpearce@Michaels-Air.broadband>
2021-09-18 15:25:46 +0100

    feat: Outline of fucntions needed added to quiz.js

---
+++ assets/js/quiz.js
@@ -0,0 +1,13 @@
+// functions
+
+//
+function createQuiz(){}
+
+function showResults(){}
+
+// To display straight away. 
+createQuiz();
+
+
+// To show results on submit
+submitButton.addEventListener('click', showResults);
